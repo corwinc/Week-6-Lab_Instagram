779d3eb9112f0d7ed08513f1102e1f65df56e74d
corwincrownover <corwincrownover@corwins-macbook-air.local>
2016-03-08 09:40:53 -0800

    Initial Commit

---
+++ Week 6 Lab_InstagramTests/Week_6_Lab_InstagramTests.swift
@@ -0,0 +1,36 @@
+//
+//  Week_6_Lab_InstagramTests.swift
+//  Week 6 Lab_InstagramTests
+//
+//  Created by Corwin Crownover on 3/8/16.
+//  Copyright © 2016 Corwin Crownover. All rights reserved.
+//
+
+import XCTest
+@testable import Week_6_Lab_Instagram
+
+class Week_6_Lab_InstagramTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
